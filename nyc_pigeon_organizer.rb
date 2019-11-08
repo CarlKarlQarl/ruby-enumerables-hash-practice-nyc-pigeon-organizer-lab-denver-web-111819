db6fe88f95c2c52275d59753f1a904397008b554
Karl Matthes <karlmatthes@gmail.com>
2019-11-08 20:27:38 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,6 +2,7 @@ def nyc_pigeon_organizer(data)
   pigeon_hash = {}
   
   pigeon_hash = collect_pigeon_names(data)
+  pigeon_hash = determine_pigeon_habitat(pigeon_hash, data)
   
   pigeon_hash
 end
@@ -18,4 +19,14 @@ def collect_pigeon_names(pigeon_data)
   end
   
   pigeon_names
+end
+
+def determine_pigeon_habitat(pigeon_names, pigeon_data)
+  pigeon_lives = pigeon_names
+  
+  pigeon_lives.each do
+    
+  end
+  
+  pigeon_lives
 end
