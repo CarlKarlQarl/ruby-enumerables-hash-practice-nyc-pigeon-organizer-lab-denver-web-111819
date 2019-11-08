f67544b497a700b03c6e2407662e450f41601c54
Karl Matthes <karlmatthes@gmail.com>
2019-11-08 20:20:42 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,21 @@
 def nyc_pigeon_organizer(data)
   pigeon_hash = {}
   
+  pigeon_hash = collect_pigeon_names(data)
+  
   pigeon_hash
+end
+
+def collect_pigeon_names(pigeon_data)
+  pigeon_names = {}
+  
+  pigeon_data[:gender][:male].length.times do |index|
+    pigeon_names[pigeon_data[:gender][:male][index]] = {}
+  end
+  
+  pigeon_data[:gender][:female].length.times do |index|
+    pigeon_names[pigeon_data[:gender][:female][index]] = {}
+  end
+  
+  pigeon_names
 end
